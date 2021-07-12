1863d749fc85512fd0fb8744149fbda3f2630850
Anand27git <anandkumar256k@gmail.com>
2021-07-12 11:19:47 +0530

    inserted the employee data

--- Employee_payroll_service.sql
+++ Employee_payroll_service.sql
@@ -1,6 +1,6 @@
 /* Welcome to Employee_payroll_services */
 
-/*UC-1*/
+/*UC-1 creation of database */
 mysql> CREATE database employee_payroll_service;
 Query OK, 1 row affected (0.18 sec)
 
@@ -22,7 +22,8 @@ mysql> show databases;
 mysql> use employee_payroll_service;
 Database changed
 
-/*UC-2*/
+/*UC-2 Table creation for employee_payroll */
+
 mysql> Create table employee_payroll (
     -> id int NOT NULL auto_increment,
     -> name varchar(100) NOT NULL,
@@ -32,7 +33,7 @@ mysql> Create table employee_payroll (
     -> );
 Query OK, 0 rows affected (2.58 sec)
 
-mysql> desc employee_payroll;
+mysql> desc employee_payro
 +--------+--------------+------+-----+---------+----------------+
 | Field  | Type         | Null | Key | Default | Extra          |
 +--------+--------------+------+-----+---------+----------------+
@@ -43,4 +44,13 @@ mysql> desc employee_payroll;
 +--------+--------------+------+-----+---------+----------------+
 4 rows in set (0.05 sec)
 
-mysql>
+/*  UC-3- Inserting employee values */
+
+mysql> INSERT INTO employee_payroll
+    -> (name, salary, start) VALUES
+    -> ('Anand',35000, '2021-07-06'),
+    -> ('Harish',38000, '2021-07-07'),
+    -> ('manu',29000, '2021-07-06');
+Query OK, 3 rows affected (0.45 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
