ad5afcc583c5f90e703c92fb74a543c1e1a52065
Anand27git <anandkumar256k@gmail.com>
2021-07-12 07:37:31 +0530

    creation of database

---
+++ Employee_payroll_service.sql
@@ -0,0 +1,23 @@
+/* Welcome to Employee_payroll_services */
+
+/*UC-1*/
+mysql> CREATE database employee_payroll_service;
+Query OK, 1 row affected (0.18 sec)
+
+mysql> show databases;
++--------------------------+
+| Database                 |
++--------------------------+
+| address_book             |
+| classicmodels            |
+| employee_payroll_service |
+| information_schema       |
+| mysql                    |
+| payroll_service          |
+| performance_schema       |
+| sys                      |
++--------------------------+
+8 rows in set (0.03 sec)
+
+mysql> use employee_payroll_service;
+Database changed
